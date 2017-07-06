0e933c1a1e3580e01a801d1ff40cd9017f0ced1e
yanyj <yyjcoder@163.com>
2017-07-06 16:39:04 +0800

    gulp

--- dist/libJS.js
+++ dist/libJS.js
@@ -1,5 +1,6 @@
 /**
- * Created by yanyj on 2017/7/5.
+ * 工具类集合，可通过libJS.XXX.xxx访问其工具类的属性
+ * @module libJS
  */
 
 (function (root, factory) {
@@ -20,16 +21,17 @@
 }(this, function () {
 
 /**
- * Created by yanyj on 2017/7/6.
+ * 公共类
+ * @constructor _lib_commonJS
  */
 
-function _commonJS() {
+function _lib_commonJS() {
 
     var _common = {
         /**
-         *
-         * @param {object} typeOfValue
-         * @param {string} type
+         *检查输入的值与类型是否匹配
+         * @param {object} typeOfValue 被检测的值
+         * @param {string} type 检测类型
          */
         typeOfValue: function (typeOfValue, type) {
             return typeof typeOfValue === type;
@@ -42,59 +44,114 @@ function _commonJS() {
 
 
 /**
- * Created by yanyj on 2017/7/5.
+ * Number工具类
+ * @constructor _lib_numberJS
  */
 
 
-var _lib_Number = function () {
+var _lib_numberJS = function () {
 
-    var commonJS = _commonJS();
 
-    /**
-     * 判断是否是数字
-     * @param {number} number
-     * @return {boolean}
-     * @private
-     */
-    function _isNumber(number) {
-        return commonJS.typeOfValue(number, "number");
-    }
+    var commonJS = _lib_commonJS();
 
     /**
      *
-     * @param {object} value
+     *  判断参数是否为数字
+     * @param {object} object 检测对象
+     * @return {boolean} true/false
+     */
+
+    function isNumber(object) {
+        return commonJS.typeOfValue(object, "number");
+    }
+
+
+
+
+    /**
+     *
+     * 将参数转化为数字 当输入的值首字母非数字时会抛出异常
+     * @param {object} value 要转换的值
      * @returns {Number}
      */
-    function _covertObjectToNumber(value) {
+    function covertObjectToNumber(value) {
         var valueStr = value.toString();
         if(!_isNumberByIndex(valueStr, 0))  {
-            throw new Error("参数非法，首字母必须为数字")
+            throw new Error("参数非法，首字母必须为数字!")
         }
         return valueStr.indexOf(".") === -1 ? parseInt(valueStr) : parseFloat(valueStr);
     }
 
+    /**
+     * 判断指定位置是否为数字
+     * @param {string} str 字符串
+     * @param {number} index 位置（从0开始）
+     * @returns {boolean}
+     */
     function _isNumberByIndex(str, index) {
 
         return !!parseInt(str.charAt(index));
     }
 
-
-
-
     return {
-        isNumber: _isNumber,
-        covertObjectToNumber: _covertObjectToNumber
+        isNumber: isNumber,
+        covertObjectToNumber: covertObjectToNumber
     };
 
 };
 
 
+
+
+
+
+
+
+
 /**
- * Created by yanyj on 2017/7/5.
+ * String工具类，可通过libJS.string 访问
+ * @constructor _lib_stringJS
+ *
  */
+function _lib_stringJS() {
+
+    var comonJS = _lib_commonJS();
+
+    /**
+     *
+    * 判断是否是字符串
+    * @param {{}} object
+    * @returns {*}
+    */
+    function isString(object) {
+        return comonJS.typeOfValue(object, 'string');
+    }
+    return {
+          isString: isString
+    };
+}
+
+
+
 
     return {
-        number: _lib_Number()
+        /**
+         * libJS中的common公共类
+         * @see {@link -lib_commonJS.html _lib_commonJS}.
+         *
+         * @property common  _lib_commonJS 通过libJS.common可访问
+         */
+        common: _lib_commonJS(),
+        /**
+         * libJS中的Number工具类
+         * @property number _lib_numberJS 通过libJS.number可访问
+         */
+        number: _lib_numberJS(),
+        /**
+         * libJS中的String工具类
+         * @property string _lib_stringJS 通过libJS.string可访问
+         */
+        string: _lib_stringJS()
     };
 
 
