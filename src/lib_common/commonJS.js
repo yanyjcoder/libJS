ff98823802474dab5f211c65a1f2d85858ad7f8c
yanyj <yyjcoder@163.com>
2017-07-10 09:21:16 +0800

    add performance test

--- src/lib_common/commonJS.js
+++ src/lib_common/commonJS.js
@@ -7,7 +7,7 @@ function _lib_commonJS() {
 
     var _common = {
         /**
-         *检查输入的值与类型是否匹配
+         *  检查输入的值与类型是否匹配
          * @param {object} typeOfValue 被检测的值
          * @param {string} type 检测类型
          */
